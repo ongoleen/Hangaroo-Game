15a421b7b5ad2958c60c56179eed3b097028a067
Casadogva <57438014+ongoleen@users.noreply.github.com>
2019-11-12 00:25:45 +0800

    Delete Hangaroo_1.py

--- Hangaroo_1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-secretWord = 'apple'
-lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
-def isWordGuessed(secretWord, lettersGuessed):
-    for letter in secretWord:
-        if letter not in lettersGuessed:
-            return False
-    return True
-print (isWordGuessed(secretWord, lettersGuessed))
