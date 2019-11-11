5f9ce7ee7e21312634281ffda47aee75847f0a7d
Casadogva <57438014+ongoleen@users.noreply.github.com>
2019-11-12 00:25:57 +0800

    Add files via upload

---
+++ Hangaroo_1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+secretWord = 'apple'
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+def isWordGuessed(secretWord, lettersGuessed):
+    for letter in secretWord:
+        if letter not in lettersGuessed:
+            return False
+    return True
+print (isWordGuessed(secretWord, lettersGuessed))
